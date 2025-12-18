bb7a8876c7f68a2dedabcf497bafa194f2e14c66
bala-amypo <bala@amypo.in>
2025-12-18 16:31:25 +0000

    User commit

--- src/main/java/com/example/demo/model/SkillRequest.java
+++ src/main/java/com/example/demo/model/SkillRequest.java
@@ -25,20 +25,3 @@ public class SkillRequest {
     public Boolean getActive() { return active; }
     public void setActive(Boolean active) { this.active = active; }
 }
-
-
-___________________
-
-
---------------------------
-
-// SkillRequestRepository.java
-
-
-----------------------
-
-
-
-
--------------
-
