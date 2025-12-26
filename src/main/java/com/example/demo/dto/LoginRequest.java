8350c1cc4c2a0c10d9e83fe75e9ee54b1ea5131f
bala-amypo <bala@amypo.in>
2025-12-26 12:57:19 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,34 +1,12 @@
 package com.example.demo.dto;
 
-public class LoginResponse {
+public class LoginRequest {
     private String email;
-    private String token;
-    private String role;
-    private Long userId;
-    
-    public LoginResponse() {}
-    
-    public LoginResponse(String email, String token) {
-        this.email = email;
-        this.token = token;
-    }
-    
-    public LoginResponse(String email, String token, String role, Long userId) {
-        this.email = email;
-        this.token = token;
-        this.role = role;
-        this.userId = userId;
-    }
+    private String password;
     
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
     
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
-    
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
-    
-    public Long getUserId() { return userId; }
-    public void setUserId(Long userId) { this.userId = userId; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
